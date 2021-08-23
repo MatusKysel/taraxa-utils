4a7b6cf17a2408541863cebff5d4ac39b0905bfb
Matus Kysel <matuskysel1@gmail.com>
2021-08-23 15:27:42 +0200

    more tuning

--- check4fork.py
+++ check4fork.py
@@ -4,10 +4,10 @@ import subprocess
 import shlex
 from time import sleep
 
-url = "http://127.0.0.1:7777"
+url = "http://127.0.0.1:9999"
 
 
-template_text = "kubectl port-forward po/%s 7777:7777"
+template_text = "kubectl port-forward po/%s 9999:7777"
 #node_template = "blockchain-testnet-consensus-node-%s"
 node_template = "blockchain-devnet-consensus-node-%s"
 number_of_nodes = 18
@@ -17,7 +17,6 @@ def get_pbft_chain_size():
     r = requests.post(url, data=json.dumps(
         {"jsonrpc": "2.0", "method": "get_pbft_chain_size", "params": {}, "id": 0}))
     return int(json.loads(r.text)["result"]["value"])
-    # return height
 
 
 def get_pbft_block_hash(block_num):
@@ -31,17 +30,20 @@ def check_for_fork():
     old_block = ''
     old_node = ''
     for idx in range(number_of_nodes + 1):
+        node = node_template % idx
+        child = subprocess.Popen(shlex.split(
+            template_text % node), stdout=subprocess.PIPE)
         try:
-            child = subprocess.Popen(shlex.split(
-            template_text % (node_template % idx)), stdout=subprocess.PIPE)
             sleep(3)
             size = get_pbft_chain_size()
+            print(node + " has pbft chain size: " + str(size))
             if(lowest_chain > size):
                 lowest_chain = size
+                old_node = node
             child.terminate()
         except:
             child.terminate()
-    print(lowest_chain)
+    print("smallest chain : " + old_node + " -> " + str(lowest_chain))
     for idx in range(number_of_nodes + 1):
         node = node_template % idx
         child = subprocess.Popen(shlex.split(
@@ -50,6 +52,8 @@ def check_for_fork():
             sleep(3)
             block = get_pbft_block_hash(lowest_chain)
             if(len(old_block) != 0 and old_block != block):
+                print("################################")
+                print("FORK FOUND!!!")
                 print(node)
                 print(block)
                 print(old_node)
